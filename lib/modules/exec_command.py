9047502eed5588a4eec56829cdb0c92773f89b5c
zhou0206cindy <109018417+zhou0206cindy@users.noreply.github.com>
2024-11-04 11:41:02 +0800

    Update exec_command.py
    added timeout arg

--- lib/modules/exec_command.py
+++ lib/modules/exec_command.py
@@ -18,7 +18,7 @@ class EXEC_COMMAND():
     def __init__(self, iWbemLevel1Login, codec):
         self.iWbemLevel1Login = iWbemLevel1Login
         self.codec = codec
-
+        self.Timeout = 5
         self.obfu = VBSObfuscator()
     
     def save_ToFile(self, hostname, content):
@@ -98,8 +98,8 @@ class EXEC_COMMAND():
             #filer_Query = r"SELECT * FROM __InstanceModificationEvent WITHIN 1 WHERE TargetInstance ISA 'Win32_PerfFormattedData_PerfOS_System'"
             #tag = executer.ExecuteVBS(vbs_content=vbs, filer_Query=filer_Query, returnTag=True)
             
-            # Wait 5 seconds for next step.
-            for i in range(5,0,-1):
+            # Wait {self.timeout} seconds for next step.
+            for i in range(self.Timeout,0,-1):
                 print(f"[+] Waiting {i}s for next step.", end="\r", flush=True)
                 time.sleep(1)
             
@@ -131,8 +131,8 @@ class EXEC_COMMAND():
             #filer_Query = r"SELECT * FROM __InstanceModificationEvent WITHIN 1 WHERE TargetInstance ISA 'Win32_PerfFormattedData_PerfOS_System'"
             #tag = executer.ExecuteVBS(vbs_content=vbs, filer_Query=filer_Query, returnTag=True)
             
-            # Wait 5 seconds for next step.
-            for i in range(5,0,-1):
+            # Wait {self.timeout} seconds for next step.
+            for i in range(self.Timeout,0,-1):
                 print(f"[+] Waiting {i}s for next step.", end="\r", flush=True)
                 time.sleep(1)
         else:
@@ -175,8 +175,8 @@ class EXEC_COMMAND():
         with open('./lib/vbscripts/RemoveTempFile.vbs') as f: vbs = f.read()
         tag = executer.ExecuteVBS(vbs_content=vbs, returnTag=True)
         
-        # Wait 5 seconds for next step.
-        for i in range(5,0,-1):
+        # Wait {self.timeout} seconds for next step.
+        for i in range(self.Timeout,0,-1):
             print(f"[+] Waiting {i}s for next step.", end="\r", flush=True)
             time.sleep(1)
 
@@ -196,7 +196,7 @@ class EXEC_COMMAND_SHELL(cmd.Cmd):
         self.save_fileName = str(int(time.time())) + ".txt"
         self.logging = False
         self.interval = 5
-        self.cwd = 'C:\Windows\System32'
+        self.cwd = 'C:\\Windows\\System32'
         self.prompt = "%s>" %self.cwd
         self.intro = '[!] Launching semi-interactive shell - Careful what you execute'
         self.history = []
